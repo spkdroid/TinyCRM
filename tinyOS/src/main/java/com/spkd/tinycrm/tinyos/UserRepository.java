ae01e5099248ccbb43b99005eb4fc6988354c7f0
Ramkumar Velmurugan <vrk1990@outlook.com>
2024-06-22 23:44:16 -0400

    spring boot backend microservice pushed :rocket:

---
+++ tinyOS/src/main/java/com/spkd/tinycrm/tinyos/UserRepository.java
@@ -0,0 +1,6 @@
+package com.spkd.tinycrm.tinyos;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
---
+++ tinyOS/src/test/java/com/spkd/tinycrm/tinyos/TinyOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spkd.tinycrm.tinyos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
